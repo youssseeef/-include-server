d2486f87c38919b30bd2073b43398731836654de
Youssef Salama <youssefhanii@hotmail.com>
2017-12-17 21:42:06 +0200

    added one more model

---
+++ models/ambulance-model-from-device.model.js
@@ -0,0 +1,11 @@
+let ambulanceModelFromDevice = {
+    carId: {
+        type: 'string',
+        required: true
+    },
+    location: {
+        type: 'string',
+        required: true
+    },
+}
+module.exports = ambulanceModelFromDevice;
