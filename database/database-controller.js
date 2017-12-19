faf77939164687da5f70dc868959d3647583ff72
Youssef Salama <youssefhanii@hotmail.com>
2017-12-19 20:20:27 +0200

    added redis config

--- database/database-controller.js
+++ database/database-controller.js
@@ -1,5 +1,5 @@
 const redis = require('redis');
-const client = redis.createClient();
+const client = redis.createClient(process.env.REDIS_URL);
 /**
  * This file is supposed to be what interacts with the database
  * Functions should mainly update items in the database.
