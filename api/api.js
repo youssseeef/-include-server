c887f26d61178f918db961363da8529188157b2a
Youssef Salama <youssefhanii@hotmail.com>
2018-01-29 12:33:24 +0200

    test

--- api/api.js
+++ api/api.js
@@ -39,7 +39,7 @@ app.post('/api/cars/request', (req, res) => {
  * This also checks when an accident happens - it does the assignment algorithm
  */
 app.post('/api/cars/update', (req, res) => {
-    console.log(req);
+    //console.log(req);
     console.log(req.body)
 
     try {
