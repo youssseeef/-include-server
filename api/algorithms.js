1d6390153a4d505635441b4a2c7bff00f8d8029c
Youssef Salama <youssefhanii@hotmail.com>
2018-04-14 05:18:48 +0200

    test

--- api/algorithms.js
+++ api/algorithms.js
@@ -35,7 +35,7 @@ function accidentOccured(affectedCarId, affectedCarData, affectedCarRoad) {
             });
             ambulanceArray.forEach((element, index) => {
                 console.log(element)
-                if (element.value['location'] != undefined && element.value['carAssigned'] == undefined) {
+                if (element.value.value['location'] != undefined && element.value.value['carAssigned'] == undefined) {
                     //shouldn't be there but that's just for the MVP demo
                     //this is the only ambulance in the system now! This should definitely be changed
                     //in a final product.
