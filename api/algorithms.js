8ba2152d7daed90caebfd1c635d2b7a8962d1a15
Youssef Salama <youssefhanii@hotmail.com>
2018-04-14 06:00:19 +0200

    fix

--- api/algorithms.js
+++ api/algorithms.js
@@ -16,45 +16,49 @@ function timeStampGenerator() {
  * @param {*The exact road the car's walking on when the accident occured. } affectedCarRoad 
  */
 function accidentOccured(affectedCarId, affectedCarData, affectedCarRoad) {
-    let location = affectedCarData['location'];
-    let accidentStatus = affectedCarData['accidentStatus'];
-    let timestamp = affectedCarData['timestamp'];
-    let ambulanceAssigned = affectedCarData['ambulanceAssigned'];
-    //Check if there is no ambulance assignment to act upon it.
-    //if there is one, that means the car has been assigned an ambulance.
-    if (ambulanceAssigned === undefined) {
-        console.log("GOT HERE!");
 
-        databaseController.getAllAmbulances((ambulances) => {
-            let ambulanceArray = [];
-            Object.keys(ambulances).forEach(ambulance => {
-                ambulanceArray.push({
-                    id: ambulance,
-                    value: ambulances[ambulance]
+    databaseController.getCarData(affectedCarId, (affectedCarData) => {
+        let location = affectedCarData['location'];
+        let accidentStatus = affectedCarData['accidentStatus'];
+        let timestamp = affectedCarData['timestamp'];
+        let ambulanceAssigned = affectedCarData['ambulanceAssigned'];
+        //Check if there is no ambulance assignment to act upon it.
+        //if there is one, that means the car has been assigned an ambulance.
+        if (ambulanceAssigned === undefined) {
+            console.log("GOT HERE!");
+
+            databaseController.getAllAmbulances((ambulances) => {
+                let ambulanceArray = [];
+                Object.keys(ambulances).forEach(ambulance => {
+                    ambulanceArray.push({
+                        id: ambulance,
+                        value: ambulances[ambulance]
+                    });
+                });
+                ambulanceArray.forEach((element, index) => {
+                    console.log(index);
+                    console.log(element.value['location']);
+                    console.log(element.value['carAssigned'] == null);
+                    console.log(element.value['location'] !== null);
+                    if ((element.value['location'] !== null) == true && (element.value['carAssigned'] == null) == true) {
+                        //shouldn't be there but that's just for the MVP demo
+                        //this is the only ambulance in the system now! This should definitely be changed
+                        //in a final product.
+                        console.log("I AM GROOT")
+                        databaseController.setAmbulanceAssignedToCar(affectedCarId, element.id);
+                        databaseController.setCarAssignedToAmbulance(element.id + "", affectedCarId);
+                    }
                 });
             });
-            ambulanceArray.forEach((element, index) => {
-                console.log(index);
-                console.log(element.value['location']);
-                console.log(element.value['carAssigned'] == null);
-                console.log(element.value['location'] !== null);
-                if ((element.value['location'] !== null) == true && (element.value['carAssigned'] == null) == true) {
-                    //shouldn't be there but that's just for the MVP demo
-                    //this is the only ambulance in the system now! This should definitely be changed
-                    //in a final product.
-                    console.log("I AM GROOT")
-                    databaseController.setAmbulanceAssignedToCar(affectedCarId, element.id);
-                    databaseController.setCarAssignedToAmbulance(element.id + "", affectedCarId);
-                }
-            });
-        });
-    }
+        }
 
-    //assign to an ambulance/service car according to error level.
-    //0- check if car is already assigned
-    //1- get all ambulances
-    //2- get the nearest ambulance
-    //3- add a database entry for assignment
+        //assign to an ambulance/service car according to error level.
+        //0- check if car is already assigned
+        //1- get all ambulances
+        //2- get the nearest ambulance
+        //3- add a database entry for assignment
+
+    });
 
 
 }
