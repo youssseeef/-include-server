9201e930b23dec84e334e88abcfa841826f9cbd2
Youssef Salama <youssefhanii@hotmail.com>
2018-04-14 08:09:08 +0200

    test

--- api/api.js
+++ api/api.js
@@ -165,11 +165,11 @@ app.post('/api/sos/updateAmbulance', (req, res) => {
             dbController.updateAmbulanceData(newAmbData, req.body.ambulanceId, (errrrr) => {
                 dbController.getAmbulanceData(req.body.ambulanceId, (ambData) => {
                     if (ambData['carAssigned'] != undefined) {
-                        res.sendStatus(200).json({
+                        res.json({
                             carAssigned: ambData['carAssigned']
                         });
                     } else {
-                        res.sendStatus(200).json({
+                        res.json({
                             none: none
                         });
 
